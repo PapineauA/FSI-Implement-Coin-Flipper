c68ea356e56121e744ae6a925562f83f14263c18
PapineauA <papineautaidan@gmail.com>
2022-08-04 21:41:08 -0700

    Uploading Penny Flipper game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,60 @@
-// TODO: Declare any global variables we need
+//Setting Variables
+let heads = 0;
+let headsPercent = 0;
+let tails = 0;
+let tailsPercent = 0;
+let total = heads + tails;
 
-
-document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
-
-    // TODO: Add event listener and handler for flip and clear buttons
-
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
-
-
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
-
-})
+document.addEventListener("DOMContentLoaded", function () {
+  //Flip Button
+  document.querySelector("#flip").addEventListener("click", function () {
+    let flipResult = Math.random();
+    console.log(flipResult);
+    if (flipResult < 0.495) {
+      //Flipping Heads
+      document.querySelector("#penny-image").src =
+        "./assets/images/penny-heads.jpg";
+      document.querySelector("#message").textContent = "You Flipped Heads!";
+      heads++;
+      total++;
+      document.querySelector("#heads").textContent = heads;
+      headsPercent = Math.round((heads / total) * 100);
+      if (heads > 0) {
+        tailsPercent = Math.round((tails / total) * 100);
+        document.querySelector("#tails-percent").textContent =
+          tailsPercent + "%";
+      }
+      document.querySelector("#heads-percent").textContent = headsPercent + "%";
+    } else {
+      //Flipping Tails
+      document.querySelector("#penny-image").src =
+        "./assets/images/penny-tails.jpg";
+      document.querySelector("#message").textContent = "You Flipped Tails!";
+      tails++;
+      total++;
+      document.querySelector("#tails").textContent = tails;
+      tailsPercent = Math.round((tails / total) * 100);
+      if (tails > 0) {
+        headsPercent = Math.round((heads / total) * 100);
+        document.querySelector("#heads-percent").textContent =
+          headsPercent + "%";
+      }
+      document.querySelector("#tails-percent").textContent = tailsPercent + "%";
+    }
+  });
+  //Clear Button
+  document.querySelector("#clear").addEventListener("click", function () {
+    heads = 0;
+    tails = 0;
+    headsPercent = 0;
+    tailsPercent = 0;
+    total = heads + tails;
+    document.querySelector("#heads").textContent = heads;
+    document.querySelector("#tails").textContent = tails;
+    document.querySelector("#heads-percent").textContent = headsPercent + "%";
+    document.querySelector("#tails-percent").textContent = tailsPercent + "%";
+    document.querySelector("#message").textContent = "Let's Get Flipping!";
+    document.querySelector("#penny-image").src =
+      "./assets/images/penny-heads.jpg";
+  });
+});
